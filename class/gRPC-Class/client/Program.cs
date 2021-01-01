d650b69816c816fad5c948352282a70055ce89bd
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-01 20:38:06 +0530

    Client connection boiler plate code

--- class/gRPC-Class/client/Program.cs
+++ class/gRPC-Class/client/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dummy;
 
 namespace client
 {
     class Program
     {
+        const string TARGET = "localhost:50051";
         static void Main(string[] args)
         {
+            Channel channel = new Channel(TARGET, ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new DummyService.DummyServiceClient(channel);
+            channel.ShutdownAsync().Wait();
+            Console.ReadLine();
         }
     }
 }
