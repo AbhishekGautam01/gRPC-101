7a2db2affbf80c38e3f02f00b35a4b95a88f1ce7
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-03 13:05:39 +0530

    Client Streaming - Client Implementation

--- class/gRPC-Class/client/Program.cs
+++ class/gRPC-Class/client/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Greet;
 using Sum;
 using Calculator;
+using System.Linq;
 
 namespace client
 {
@@ -21,50 +22,61 @@ namespace client
             #region Greeting-Unary
             var greetingClient = new GreetingService.GreetingServiceClient(channel);
             //It looks like we are directly calling the function but this call will happen over http/2 and will goto server. 
-            var greeting = new Greeting()
+            var greetingRequest = new Greeting()
             {
                 FirstName = "Abhishek",
                 LastName = "Gautam"
             };
             var request = new GreetingRequest()
             {
-                Greeting = greeting
+                Greeting = greetingRequest
             };
-            var greetingResponse = greetingClient.Greet(request);
-            Console.WriteLine(greetingResponse.Result);
+            var unaryGreetingResponse = greetingClient.Greet(request);
+            Console.WriteLine(unaryGreetingResponse.Result);
             #endregion
             #region ServerStreaming 
-            var streamRequest = new GreetingManyTimesRequest() { Greeting = greeting };
-            var streamResponse = greetingClient.GreetManyTimes(streamRequest);
-            while(await streamResponse.ResponseStream.MoveNext())
+            var serverStreamRequest = new GreetingManyTimesRequest() { Greeting = greetingRequest };
+            var serverStreamResponse = greetingClient.GreetManyTimes(serverStreamRequest);
+            while(await serverStreamResponse.ResponseStream.MoveNext())
             {
-                Console.WriteLine(streamResponse.ResponseStream.Current.Result);
+                Console.WriteLine(serverStreamResponse.ResponseStream.Current.Result);
                 await Task.Delay(200);
             }
             #endregion
             #region CalculatorService-Streaming
             var calculatorClient = new calculator.calculatorClient(channel);
-            var decomposePrime = new PrimeDecompositionRequest()
+            var decomposePrimeRequest = new PrimeDecompositionRequest()
             {
                 Input = 120
             };
-            var decomposePrimeResponse = calculatorClient.PrimeDecomposition(decomposePrime);
+            var decomposePrimeResponse = calculatorClient.PrimeDecomposition(decomposePrimeRequest);
             while(await decomposePrimeResponse.ResponseStream.MoveNext())
             {
                 Console.WriteLine(decomposePrimeResponse.ResponseStream.Current.Result);
                 await Task.Delay(200);
             }
             #endregion
+            #region Client Streaming - Long Greet 
+            var longGreetRequest = new LongGreetRequest() { Greeting = greetingRequest };
+            var longGreetStream = greetingClient.LongGreet();
+            foreach (int i in Enumerable.Range(1, 10))
+            {
+                await longGreetStream.RequestStream.WriteAsync(longGreetRequest);
+            }
+            await longGreetStream.RequestStream.CompleteAsync();
+            var longGreetResponse = await longGreetStream.ResponseAsync;
+            Console.WriteLine(longGreetResponse);
+            #endregion
             #region SUM-Unary
             var sumClient = new SumService.SumServiceClient(channel);
-            var add = new Addition()
+            var additionRequest = new Addition()
             {
                 First = 10,
                 Second = 3
             };
             var sumRequest = new SumRequest()
             {
-                Sum = add
+                Sum = additionRequest
             };
             var sumResponse = sumClient.Sum(sumRequest);
             Console.WriteLine(sumResponse);
