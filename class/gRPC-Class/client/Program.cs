2ec49eda93e0c98dd8d5bd064716eec995ffdb57
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-03 12:05:22 +0530

    Stream Request Impl

--- class/gRPC-Class/client/Program.cs
+++ class/gRPC-Class/client/Program.cs
@@ -9,15 +9,15 @@ namespace client
     class Program
     {
         const string TARGET = "localhost:50051";
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Channel channel = new Channel(TARGET, ChannelCredentials.Insecure);
-            channel.ConnectAsync().ContinueWith((task) =>
+            await channel.ConnectAsync().ContinueWith((task) =>
             {
                 if (task.Status == TaskStatus.RanToCompletion)
                     Console.WriteLine("The client connected successfully");
             });
-
+            #region Greeting-Unary
             var greetingClient = new GreetingService.GreetingServiceClient(channel);
             //It looks like we are directly calling the function but this call will happen over http/2 and will goto server. 
             var greeting = new Greeting()
@@ -31,7 +31,17 @@ namespace client
             };
             var greetingResponse = greetingClient.Greet(request);
             Console.WriteLine(greetingResponse.Result);
-
+            #endregion
+            #region ServerStreaming 
+            var streamRequest = new GreetingManyTimesRequest() { Greeting = greeting };
+            var streamResponse = greetingClient.GreetManyTimes(streamRequest);
+            while(await streamResponse.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(streamResponse.ResponseStream.Current.Result);
+                await Task.Delay(200);
+            }
+            #endregion
+            #region SUM-Unary
             var sumClient = new SumService.SumServiceClient(channel);
             var add = new Addition()
             {
@@ -44,7 +54,7 @@ namespace client
             };
             var sumResponse = sumClient.Sum(sumRequest);
             Console.WriteLine(sumResponse);
-
+            #endregion
             channel.ShutdownAsync().Wait();
             Console.ReadLine();
         }
